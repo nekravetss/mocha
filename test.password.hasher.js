cc8ddbd2c2ab6792947920c1eddfd35882b55744
nekravetss <artem.v.kravets@ukd.edu.ua>
2025-04-29 13:04:21 +0300

    Create test.password.hasher.js

---
+++ test.password.hasher.js
@@ -0,0 +1,10 @@
+const assert = require('assert');
+const sinon = require('sinon');
+const prompt = require('prompt');
+const bcrypt = require('bcrypt');
+const { getHash } = require('./passwordHasher');
+
+describe('Password Hasher', function () {
+  it('повертає хеш при однакових паролях', async function () {
+    const promptStub = sinon.stub(prompt, 'get');
+    promptStub.resolves({ passwo
